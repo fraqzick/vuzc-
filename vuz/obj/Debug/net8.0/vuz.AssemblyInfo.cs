995bac08de0aeecab99b52104310e53949725315
fraqzick <akhmedshin.ainur06@gmail.com>
2024-09-17 09:50:06 +0300

    Лабораторная рвбота №2

--- vuz/obj/Debug/net8.0/vuz.AssemblyInfo.cs
+++ vuz/obj/Debug/net8.0/vuz.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vuz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e07ff108a1a7895e7529b83ae1baa804e41e3dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58758cf03f648f6deec8c39bd38fa63605af6307")]
 [assembly: System.Reflection.AssemblyProductAttribute("vuz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vuz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
